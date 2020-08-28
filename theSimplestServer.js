8a5f55a30af6af6eab08adc10b635af814f5b5c4
Andy <lapwailau@gmail.com>
2020-08-28 22:14:46 +1200

    the simplest server

---
+++ theSimplestServer.js
@@ -0,0 +1,13 @@
+// 1. create dependency packages file - npm init -y
+// 2. install express framework - npm inistall express
+// 3. create git ignore file ".git ignore"
+// 4. node theSimpleServer.js to run the server
+let express = require("express");
+
+let app = express();
+
+app.get("/", function (req, res) {
+  res.send("The Simplest Server is Running....");
+});
+
+app.listen(3000);
